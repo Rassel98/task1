c306abaf118a3fcb7d59b4d2d2173d22cddbd2fc
rassel <indonesiarassel@gmail.com>
2023-12-23 15:47:25 +0600

    database connected and complete crud operation

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:task_app/app/data/interface/task_interface.dart';
 import 'package:task_app/app/data/models/task_model.dart';
-import '../../../data/db_helper/local_db_helper.dart';
+import 'package:task_app/app/utils/helpers.dart';
+import '../../../data/db/local_db_helper.dart';
+import '../../../data/repository/task_repository.dart';
 
 class HomeController extends GetxController {
   RxBool isStatus = false.obs;
   RxBool isLoading = true.obs;
-
+  late TaskInterface taskInterface;
   GlobalKey<FormState> formKeys = GlobalKey<FormState>();
 
   final titleController = TextEditingController();
   final descriptionController = TextEditingController();
   final _taskList = <TaskModel>[].obs;
   List<TaskModel> get getTaskList => _taskList;
+  late TaskRepository taskRepository;
 
   @override
   void onInit() {
     super.onInit();
-    Future.delayed(const Duration(seconds: 5), () {
-      isLoading(false);
-    });
+    taskRepository = TaskRepository();
+    getAllTask();
   }
 
   @override
@@ -30,6 +33,87 @@ class HomeController extends GetxController {
   @override
   void onClose() {
     super.onClose();
-    LocalDBHelper.instance.close();
+    titleController.clear();
+    descriptionController.clear();
+    close();
   }
+
+  void insertTask() async {
+    if (!formKeys.currentState!.validate()) {
+      return;
+    }
+    TaskModel taskModel = TaskModel(
+      title: titleController.text,
+      description: descriptionController.text,
+      status: isStatus.value,
+      createdTime: DateTime.now(),
+    );
+
+    int insertedId = await taskRepository.insertTask(taskModel);
+    if (insertedId > 0) {
+      taskModel.id = insertedId;
+      taskModel.id = insertedId;
+      _taskList.add(taskModel);
+      showMessage(title: "Success !!", message: "Data inserted successfully.", duration: 3);
+      print('Data inserted successfully. Inserted ID: $insertedId');
+    } else {
+      showMessage(
+        title: "Error",
+        message: "Failed to insert data.",
+      );
+      print('Failed to insert data.');
+    }
+  }
+
+  void updateTask(TaskModel model, int index) async {
+    if (!formKeys.currentState!.validate()) {
+      return;
+    }
+    TaskModel taskModel = TaskModel(
+      id: model.id,
+      title: titleController.text,
+      description: descriptionController.text,
+      status: isStatus.value,
+      createdTime: model.createdTime,
+    );
+    int insertedId = await taskRepository.updateTask(taskModel);
+    if (insertedId > 0) {
+      _taskList.removeAt(index);
+      _taskList.insert(index, taskModel);
+      showMessage(title: "Success !!", message: "Task update successfully.", duration: 3);
+    } else {
+      showMessage(
+        title: "Error",
+        message: "Failed to update task.",
+      );
+      print('Failed to update Task.');
+    }
+  }
+
+  void deleteTask(TaskModel taskModel) async {
+    int delete = await taskRepository.deleteTask(taskModel.id!);
+    if (delete > 0) {
+      _taskList.remove(taskModel);
+      showMessage(title: "Success !!", message: "Task delete successfully.", duration: 3);
+    } else {
+      showMessage(
+        title: "Error",
+        message: "Failed to delete task.",
+      );
+    }
+  }
+
+  void getAllTask() async {
+    var response = await taskRepository.getAllTasks();
+    if (response.isNotEmpty) {
+      print('All tasks:');
+      _taskList.addAll(response);
+      isLoading(false);
+    } else {
+      isLoading(false);
+      print('No tasks found in the database.');
+    }
+  }
+
+  void close() => taskRepository.close();
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:task_app/app/modules/home/views/new_task_view.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -19,93 +19,164 @@ class HomeView extends GetView<HomeController> {
             )
           : controller.getTaskList.isEmpty
               ? const Center(
-                  child: Text("No Data Found"),
+                  child: Text("No Data Found !!"),
                 )
               : ListView.builder(
                   itemCount: controller.getTaskList.length,
                   itemBuilder: (context, index) {
                     final model = controller.getTaskList[index];
-                    return Stack(
-                      children: [
-                        Container(
+                    return Dismissible(
+                      key: Key(model.id.toString()),
+                      confirmDismiss: (direction) async {
+                        return await showCupertinoDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return CupertinoAlertDialog(
+                              title: const Text('Confirm Deletion'),
+                              content: const Text('Are you sure you want to delete task?'),
+                              actions: [
+                                CupertinoDialogAction(
+                                  onPressed: () {
+                                    Navigator.of(context).pop(false);
+                                  },
+                                  child: const Text('Cancel'),
+                                ),
+                                CupertinoDialogAction(
+                                  onPressed: () {
+                                    controller.deleteTask(model);
+                                    Navigator.of(context).pop(true);
+                                  },
+                                  isDestructiveAction: true,
+                                  child: const Text('Delete'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      direction: DismissDirection.endToStart,
+                      background: Container(
                           margin: const EdgeInsets.all(5),
                           padding: const EdgeInsets.all(10),
-                          alignment: Alignment.center,
                           decoration: BoxDecoration(
-                            color: Colors.white,
-                            //color: Colors.white,
+                            color: Colors.red,
                             borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: const Color(0xffD4D5D6).withOpacity(0.7),
-                                spreadRadius: 0,
-                                blurStyle: BlurStyle.outer,
-                                blurRadius: 20,
-                                offset: const Offset(0, 0),
-                              ),
-                            ],
                           ),
-                          child: Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisAlignment: MainAxisAlignment.start, children: [
-                            RichText(
-                              text: TextSpan(
-                                text: 'Title : ',
-                                style: const TextStyle(fontSize: 16, fontFamily: 'muli', fontWeight: FontWeight.w600, color: Color(0xFF404144)),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: "task title",
-                                    style:
-                                        const TextStyle(fontFamily: 'HindSiliguri', fontSize: 14, fontWeight: FontWeight.w300, color: Colors.black54),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            RichText(
-                              text: TextSpan(
-                                text: 'Description : ',
-                                style: const TextStyle(fontSize: 16, fontFamily: 'muli', fontWeight: FontWeight.w600, color: Color(0xFF404144)),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: "task description jdkgznkdgd gfdigfd gifdg fdgfdujgfd gfdujfd gdfjgfd gj",
-                                    style:
-                                        const TextStyle(fontFamily: 'HindSiliguri', fontSize: 14, fontWeight: FontWeight.w300, color: Colors.black54),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ]),
-                        ),
-                        Positioned(
-                          right: 10,
-                          top: 10,
-                          child: TextButton(
-                            // onPressed: () => Get.to(
-                            //       () =>  UpdateShopUser(userProfile: model,),
-                            //   transition: Transition.leftToRight,
-                            //   duration: const Duration(milliseconds: 360),
-                            //   curve: Curves.easeInOut,
-                            // ),
-                            onPressed: () => print,
-                            style: ButtonStyle(
-                              minimumSize: MaterialStateProperty.all(const Size(40, 20)),
-                              shape: MaterialStateProperty.all(const RoundedRectangleBorder(
-                                side: BorderSide(color: Colors.pinkAccent, width: 1.5),
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(12),
-                                  topRight: Radius.circular(12),
+                          alignment: Alignment.center,
+                          child: const Text("You are deleting this task")),
+                      child: Stack(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.all(5),
+                            padding: const EdgeInsets.all(10),
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: const Color(0xffD4D5D6).withOpacity(0.7),
+                                  spreadRadius: 0,
+                                  blurStyle: BlurStyle.outer,
+                                  blurRadius: 20,
+                                  offset: const Offset(0, 0),
                                 ),
-                              )),
+                              ],
                             ),
-                            child: const Text('Edit', style: TextStyle(fontFamily: 'muli', fontSize: 12, fontWeight: FontWeight.bold)),
+                            child: Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisAlignment: MainAxisAlignment.start, children: [
+                              RichText(
+                                text: TextSpan(
+                                  text: 'Title : ',
+                                  style: const TextStyle(fontSize: 16, fontFamily: 'muli', fontWeight: FontWeight.w600, color: Color(0xFF404144)),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: model.title ?? "",
+                                      style: const TextStyle(fontFamily: 'HindSiliguri', fontSize: 14, fontWeight: FontWeight.w300, color: Colors.black54),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              RichText(
+                                text: TextSpan(
+                                  text: 'Description : ',
+                                  style: const TextStyle(fontSize: 16, fontFamily: 'muli', fontWeight: FontWeight.w600, color: Color(0xFF404144)),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: model.description ?? "",
+                                      style: const TextStyle(fontFamily: 'HindSiliguri', fontSize: 14, fontWeight: FontWeight.w300, color: Colors.black54),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Row(
+                                children: [
+                                  const Text(
+                                    'Status : ',
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black),
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                                    decoration: BoxDecoration(
+                                      color: model.status ? const Color(0xff268A58) : Colors.red,
+                                      borderRadius: BorderRadius.circular(5),
+                                      border: Border.all(width: 0.5, color: Colors.pink),
+                                    ),
+                                    child: Text(
+                                      model.status ? 'Active' : "Inactive",
+                                      style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.white),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ]),
                           ),
-                        )
-                      ],
+                          Positioned(
+                            right: 10,
+                            top: 0,
+                            child: TextButton(
+                              onPressed: () => Get.to(
+                                () => NewTaskView(
+                                  taskModel: model,
+                                  idx: index,
+                                ),
+                                transition: Transition.leftToRight,
+                                duration: const Duration(milliseconds: 260),
+                                curve: Curves.easeInOut,
+                              ),
+                              style: ButtonStyle(
+                                minimumSize: MaterialStateProperty.all(const Size(35, 25)),
+                                maximumSize: MaterialStateProperty.all(const Size(43, 28)),
+                                shape: MaterialStateProperty.all(const RoundedRectangleBorder(
+                                  side: BorderSide(color: Colors.pinkAccent, width: 1.5),
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(12),
+                                    topRight: Radius.circular(12),
+                                  ),
+                                )),
+                              ),
+                              child: const Text('Edit', style: TextStyle(fontSize: 10, fontWeight: FontWeight.w600)),
+                            ),
+                          )
+                        ],
+                      ),
                     );
                   },
                 )),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: FloatingActionButton(
-        onPressed: () => print,
-        child: Icon(Icons.add),
+        onPressed: () => Get.to(
+          () => const NewTaskView(),
+          transition: Transition.leftToRight,
+          duration: const Duration(milliseconds: 260),
+          curve: Curves.easeInOut,
+        ),
+        child: const Icon(Icons.add),
       ),
     );
   }
